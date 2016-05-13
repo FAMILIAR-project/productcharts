04d827052283b2da6e27967c9f51c76b0620e379
Mathieu Acher <mathieu.acher@irisa.fr>
2016-05-13 10:42:31 +0200

    Tests enhancement (pokemon, Nokia, all products) and easier to parameterize thanks to a refactoring (btw I found an interesting case for code cloning as part of the refactoring process)

--- src/test/java/org/opencompare/CellUniformityTest.java
+++ src/test/java/org/opencompare/CellUniformityTest.java
@@ -22,10 +22,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.logging.Logger;
 
 import static org.junit.Assert.assertEquals;
@@ -39,7 +36,8 @@ public class CellUniformityTest {
 
 
     Logger _log = Logger.getLogger("CellUniformityTest");
-    private String chartTargetFolder = "outputAll";
+
+
 
 
     public PCM mkPokemonPCM() throws IOException {
@@ -95,10 +93,74 @@ public class CellUniformityTest {
     }
 
 
+    @Test
+    public void testProductChart1() throws Exception {
+
+
+        String chartTargetFolder = "outputNokia";
+        String fileName = "pcms/Comparison_of_Nikon_DSLR_cameras_0.pcm";
+
+
+        // precondition: output folder exist
+        File f = new File(chartTargetFolder);
+        if (!f.exists() || !f.isDirectory())
+            assertTrue(f.mkdir());
+
+        _buildRandomProductChart(PCMUtils.loadPCM(fileName), "NokiaRandom2", chartTargetFolder, 2);
+        _buildRandomProductChart(PCMUtils.loadPCM(fileName), "NokiaRandom3", chartTargetFolder, 3);
+    }
 
 
     @Test
-    public void testProductCharts() throws Exception {
+    public void testProductChart2() throws Exception {
+
+
+        String chartTargetFolder = "outputPokemon";
+
+        // precondition: output folder exist
+        File f = new File(chartTargetFolder);
+        if (!f.exists() || !f.isDirectory())
+            assertTrue(f.mkdir());
+
+        _buildRandomProductChart(mkPokemonPCM(), "Pokemon2", chartTargetFolder, 2);
+        _buildRandomProductChart(mkPokemonPCM(), "Pokemon3", chartTargetFolder, 3);
+
+    }
+
+
+
+    @Test
+    public void testProductCharts1() throws Exception {
+
+
+        String chartTargetFolder = "outputAll";
+        int chartDimension = 2; // X, Y, or Z (bubble size)
+
+        _buildRandomProductCharts(chartTargetFolder, chartDimension);
+
+    }
+
+    @Test
+    public void testProductCharts2() throws Exception {
+
+
+        String chartTargetFolder = "outputAllBubble";
+        int chartDimension = 3; // X, Y, or Z (bubble size)
+
+        _buildRandomProductCharts(chartTargetFolder, chartDimension);
+
+    }
+
+    /**
+     * Generate a produch chart for each PCM in the folder (hard-coded below).
+     * The output folder is chartTargetFolder
+     * user can specify if she wants two or three dimensions
+     * @param chartTargetFolder
+     * @param chartDimension
+     * @throws IOException
+     */
+    private void _buildRandomProductCharts(String chartTargetFolder, int chartDimension) throws IOException {
+        assertTrue(chartDimension == 2 || chartDimension == 3);
 
         // precondition: output folder exist
         File f = new File(chartTargetFolder);
@@ -127,49 +189,86 @@ public class CellUniformityTest {
                 assertNotNull(pcm);
                 i++;
 
-
-
-                MustacheEngine engine = MustacheEngineBuilder
-                        .newBuilder()
-                        .addTemplateLocator(new FileSystemTemplateLocator(1, "template/", "html"))
-                        .build();
-
-                Collection<String> candidateFts = _collectUniformAndNumericalFeatures(pcm);
-                if (candidateFts.size() < 2) {
-                    _log.warning("Impossible to produce a product chart for PCM: " + pcmFile.getName());
+                if (!_buildRandomProductChart(pcm, pcmFile.getName(), chartTargetFolder, chartDimension))
                     break;
-                }
-
-                Iterator<String> it = candidateFts.iterator();
-                String xFeature = it.next();
-                String yFeature = it.next();
-
-                ProductChartBuilder pchart = new ProductChartBuilder(pcm, xFeature, yFeature);
-
-
-
-                String data = pchart.buildData();
-
-                Mustache mustache = engine.getMustache("index");
-
-
-
-
-                String output = mustache.render(ImmutableMap.<String, Object>of("pcmData", data,
-                        "pcmTitle", pcmFile.getName(),
-                        "xFeature", xFeature,
-                        "yFeature", yFeature
-                ));
-
-                FileWriter fw = new FileWriter(new File(chartTargetFolder + "/" + "index-" + pcmFile.getName() + ".html"));
-                fw.write(output);
-                fw.close();
             }
         }
 
 
+    }
+
+    /**
+     *
+     * build a product chart for a specific PCM with a random strategy for picking random, numerical features
+     * @param pcm
+     * @param pcmName
+     * @param chartTargetFolder
+     * @param chartDimension
+     * @return
+     * @throws IOException
+     */
+    private boolean _buildRandomProductChart(PCM pcm, String pcmName, String chartTargetFolder, int chartDimension) throws IOException {
 
 
+        MustacheEngine engine = MustacheEngineBuilder
+                .newBuilder()
+                .addTemplateLocator(new FileSystemTemplateLocator(1, "template/", "html"))
+                .build();
+
+        Collection<String> candidateFts = _collectUniformAndNumericalFeatures(pcm);
+
+
+
+        if (candidateFts.size() < chartDimension) {
+            _log.warning("Impossible to produce a product chart for PCM: " + pcmName);
+            return false;
+        }
+
+        List<String> lCandidateFts = new ArrayList<String>(candidateFts);
+        Collections.shuffle(lCandidateFts);
+        List<String> lfts = lCandidateFts.subList(0, chartDimension);
+
+        assertTrue(lfts.size() <= chartDimension);
+        String xFeature = lfts.get(0);
+        String yFeature = lfts.get(1);
+        String zFeature = null;
+
+        ProductChartBuilder pchart = null ;
+        if (chartDimension == 2)
+            pchart = new ProductChartBuilder(pcm, xFeature, yFeature);
+        else {
+            assertEquals(3, chartDimension);
+            zFeature = lfts.get(2);
+            pchart = new ProductChartBuilder(pcm, xFeature, yFeature, zFeature);
+        }
+
+
+
+        String data = pchart.buildData();
+
+        Mustache mustache = engine.getMustache("index");
+
+
+        Map<String, Object> valueTemplates = ImmutableMap.<String, Object>of("pcmData", data,
+                "pcmTitle", pcmName,
+                "xFeature", xFeature,
+                "yFeature", yFeature
+        );
+        if (chartDimension == 3) {
+            // TODO: weird because yep this is an immutable (unmodifiable) which is OK btw
+            // and we can't use put or methods to basically add new key, values
+            // problem is that method "of" only supports 4 pairs
+            valueTemplates = new HashMap<String, Object>(valueTemplates);
+            valueTemplates.put("zFeature", zFeature);
+        }
+
+        String output = mustache.render(valueTemplates);
+
+        FileWriter fw = new FileWriter(new File(chartTargetFolder + "/" + "index-" + pcmName + ".html"));
+        fw.write(output);
+        fw.close();
+
+        return true;
 
     }
 
